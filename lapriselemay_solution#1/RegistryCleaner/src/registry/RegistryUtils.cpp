ac1ea992549e6b89d73d75db0e02de010f0a2a76
1231016Felix <1231016@cstj.qc.ca>
2025-12-26 01:51:14 -0500

    remove registrycleaner

--- lapriselemay_solution#1/RegistryCleaner/src/registry/RegistryUtils.cpp
+++
@@ -1,8 +0,0 @@
-// RegistryUtils.cpp - Registry Utility Functions Implementation
-#include "pch.h"
-#include "registry/RegistryUtils.h"
-
-// All utility functions are inline in the header
-namespace RegistryCleaner::Registry::Utils {
-    // Implementation file for potential future non-inline functions
-} // namespace RegistryCleaner::Registry::Utils
--- lapriselemay_solution#1/RegistryCleaner/src/scanners/BaseScanner.cpp
+++
@@ -1,7 +0,0 @@
-// BaseScanner.cpp - Base Scanner Implementation
-#include "pch.h"
-#include "scanners/BaseScanner.h"
-
-namespace RegistryCleaner::Scanners {
-    // Base implementation - virtual methods are in derived classes
-} // namespace RegistryCleaner::Scanners
